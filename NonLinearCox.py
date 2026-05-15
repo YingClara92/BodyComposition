942b71a47d245f3bf6b9e6d58f5916681d0cc259
YingClara92 <53481254+YingClara92@users.noreply.github.com>
2026-05-15 15:59:44 +0800

    Update NonLinearCox.py

--- NonLinearCox.py
+++ NonLinearCox.py
@@ -12,21 +12,12 @@ from sklearn.preprocessing import StandardScaler
 # speed_muscle_body_ratio -0.5
 # speed_torso_body_ratio -0.5
 
-df = pd.read_csv('ML_survival.csv')
-target = 'speed_ML_ratio'#
+# df = pd.read_csv('ML_survival.csv')
+# target = 'speed_ML_ratio'#
 
-# df = pd.read_csv('BC_speed1.csv')
-# target = 'speed_muscle_body_ratio'#'speed_subcutaneous_fat_body_ratio'#'speed_torso_body_ratio'#
-# # print(len(df))
-# df = df[["survival_months", "deceased", target]]
-# plt.hist(df[target], bins=30, edgecolor='black')
-# plt.show()
+df = pd.read_csv('BC_speed1.csv')
+target = 'speed_muscle_body_ratio'#'speed_subcutaneous_fat_body_ratio'#'speed_torso_body_ratio'#
 df = df[["survival_months", "deceased", target]]
-
-
-# cph_reduced = CoxPHFitter(penalizer=0.01)
-# cph_reduced.fit(df, duration_col='survival_months', event_col='deceased')
-# cph_reduced.print_summary()
 
 
 spline_terms = dmatrix("bs(speed_ML_ratio, df=3, include_intercept=False)", {"speed_ML_ratio": df[target]}, return_type='dataframe')
@@ -40,7 +31,7 @@ cph.fit(df_spline, duration_col='survival_months', event_col='deceased')
 cph.print_summary()
 
 # df_reduced = df[["survival_months", "deceased"]]  # No spline, no speed_ML_ratio
-# cph_reduced = CoxPHFitter()
+# cph_reduced = CoxPHFitter(penalizer=0.01)
 # cph_reduced.fit(df_reduced, duration_col='survival_months', event_col='deceased')
 #
 # # Perform likelihood ratio test
@@ -73,16 +64,16 @@ partial_hazard = cph.predict_partial_hazard(df_plot)
 from lifelines import KaplanMeierFitter
 from lifelines.plotting import add_at_risk_counts
 
-# cluster_num = 2
-# df['cluster_' + target] = 0
-# df['cluster_' + target] = np.where(df[target] <= -0.5, 1, df['cluster_' + target])
-# target_names = ['SKM Preserved','SKM Loss']
-
-cluster_num = 3
+cluster_num = 2
 df['cluster_' + target] = 0
-df['cluster_' + target] = np.where(df[target] < -0.25, 1, df['cluster_' + target])
-df['cluster_' + target] = np.where(df[target] > 0.28, 2, df['cluster_' + target])
-target_names = ['Cardiac Stable','Cardiac Atrophy', 'Cardiac Hypertrophy']
+df['cluster_' + target] = np.where(df[target] <= -0.42, 1, df['cluster_' + target])
+target_names = ['SKM Preserved','SKM Loss']
+
+# cluster_num = 3
+# df['cluster_' + target] = 0
+# df['cluster_' + target] = np.where(df[target] < -0.29, 1, df['cluster_' + target])
+# df['cluster_' + target] = np.where(df[target] > 0.32, 2, df['cluster_' + target])
+# target_names = ['Cardiac Stable','Cardiac Atrophy', 'Cardiac Hypertrophy']
 
 for cluster in range(cluster_num):
     data_c = df[df['cluster_' + target] == cluster][target]
@@ -147,67 +138,65 @@ results = logrank_test(df[df['cluster_' + target] == 1]['survival_months'],
 print('logrank test p',results.p_value)
 
 
-results = logrank_test(df[df['cluster_' + target] == 2]['survival_months'],
-                    df[df['cluster_' + target] == 0]['survival_months'],
-                    df[df['cluster_' + target] == 2]['deceased'],
-                    df[df['cluster_' + target] == 0]['deceased'], alpha=0.95)
-print('logrank test p',results.p_value)
+# results = logrank_test(df[df['cluster_' + target] == 2]['survival_months'],
+#                     df[df['cluster_' + target] == 0]['survival_months'],
+#                     df[df['cluster_' + target] == 2]['deceased'],
+#                     df[df['cluster_' + target] == 0]['deceased'], alpha=0.95)
+# print('logrank test p',results.p_value)
+#
+#
+#
+# results = logrank_test(df[df['cluster_' + target] == 1]['survival_months'],
+#                     df[df['cluster_' + target] == 2]['survival_months'],
+#                     df[df['cluster_' + target] == 1]['deceased'],
+#                     df[df['cluster_' + target] == 2]['deceased'], alpha=0.95)
+# print('logrank test p',results.p_value)
 
 
-
-results = logrank_test(df[df['cluster_' + target] == 1]['survival_months'],
-                    df[df['cluster_' + target] == 2]['survival_months'],
-                    df[df['cluster_' + target] == 1]['deceased'],
-                    df[df['cluster_' + target] == 2]['deceased'], alpha=0.95)
-print('logrank test p',results.p_value)
-
-
-# when plot the partial hazard, use color to distinguish the clusters, for example, for ML_range within [-0.5, -0.25], use red color, for ML_range within [-0.25, 0.28], use green color, and for ML_range > 0.28, use blue color
-plt.figure(figsize=(8, 6))
-for i in range(cluster_num):
-    mask = (ML_range < -0.25) if i == 1 else (ML_range > 0.28) if i == 2 else (ML_range >= -0.25) & (ML_range <= 0.28)
-    plt.plot(ML_range[mask], partial_hazard[mask], label=target_names[i])
-
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
-plt.xlabel(r'$v_{\mathrm{myo}}$(%)', fontsize=20)
-plt.ylabel('Partial Hazard', fontsize=20)
-# plt.axhline(y=1, color='gray', linestyle='--', linewidth=1)
-plt.title('Effect of $v_{\mathrm{myo}}$ on Hazard', fontsize=20)
-ax = plt.gca()
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.legend(frameon=False, fontsize=20)
-plt.tight_layout()
-plt.show()
-
-# # when plot the partial hazard, use color to distinguish the clusters, for example, for ML_range <= -0.5, use red color, for ML_range between >= -0.5 ause green color
 # plt.figure(figsize=(8, 6))
 # for i in range(cluster_num):
-#     mask = (ML_range <= -0.5) if i == 1 else (ML_range > -0.5)
+#     mask = (ML_range < -0.29) if i == 1 else (ML_range > 0.32) if i == 2 else (ML_range >= -0.29) & (ML_range <= 0.32)
 #     plt.plot(ML_range[mask], partial_hazard[mask], label=target_names[i])
-# ## set font size for x tick labels
+#
 # plt.xticks(fontsize=20)
 # plt.yticks(fontsize=20)
-# ## horizontal line at y=1
-# # plt.axhline(y=1, color='gray', linestyle='--', linewidth=1)
-# plt.xlabel(r'$v_{\mathrm{muscle}}$(%)', fontsize=20)
+# plt.xlabel(r'$v_{\mathrm{myo}}$(%)', fontsize=20)
 # plt.ylabel('Partial Hazard', fontsize=20)
-# plt.title('Effect of $v_{\mathrm{muscle}}$ on Hazard', fontsize=20)
-# ## don't have frame for the plot
+# # plt.axhline(y=1, color='gray', linestyle='--', linewidth=1)
+# plt.title('Effect of $v_{\mathrm{myo}}$ on Hazard', fontsize=20)
 # ax = plt.gca()
 # ax.spines['top'].set_visible(False)
 # ax.spines['right'].set_visible(False)
 # ax.legend(frameon=False, fontsize=20)
-# ## dont have frame for the legend
-#
 # plt.tight_layout()
 # plt.show()
+
+plt.figure(figsize=(8, 6))
+for i in range(cluster_num):
+    mask = (ML_range <= -0.42) if i == 1 else (ML_range > -0.42)
+    plt.plot(ML_range[mask], partial_hazard[mask], label=target_names[i])
+## set font size for x tick labels
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+## horizontal line at y=1
+# plt.axhline(y=1, color='gray', linestyle='--', linewidth=1)
+plt.xlabel(r'$v_{\mathrm{muscle}}$(%)', fontsize=20)
+plt.ylabel('Partial Hazard', fontsize=20)
+plt.title('Effect of $v_{\mathrm{muscle}}$ on Hazard', fontsize=20)
+## don't have frame for the plot
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.legend(frameon=False, fontsize=20)
+## dont have frame for the legend
+
+plt.tight_layout()
+plt.show()
 
 ## time at 30 months
 print("Survival probability at 30 months for group 0:", kmf_models[0].survival_function_at_times(30))
 print("Survival probability at 30 months for group 1:", kmf_models[1].survival_function_at_times(30))
-print("Survival probability at 30 months for group 2:", kmf_models[2].survival_function_at_times(30))
+# print("Survival probability at 30 months for group 2:", kmf_models[2].survival_function_at_times(30))
 ## time at 32 months
 # print("Survival probability at 32 months for group 0:", kmf_models[0].survival_function_at_times(32))
 # print("Survival probability at 32 months for group 1:", kmf_models[1].survival_function_at_times(32))
@@ -220,4 +209,3 @@ print("Survival probability at 30 months for group 2:", kmf_models[2].survival_f
 # print("Confidence intervals at 30 months for group 0:", kmf_models[0].confidence_interval_(30))
 # print("Confidence intervals at 30 months for group 1:", kmf_models[1].confidence_interval_(30))
 # print("Confidence intervals at 30 months for group 2:", kmf_models[2].confidence_interval_(30))
-#
